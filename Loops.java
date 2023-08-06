de290b41fa416dff687d567bbd115471cd4daeaa
Akshay <akshaykashyap0545@gmail.com>
2023-08-06 12:29:32 +0530

    dev branch change 1

---
+++ Loops.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Loops
+{
+    public static void main (String args[])
+    {
+        System.out.println("Loops in java");
+        System.out.println("IN DEV BRANCH");
+    }
+}
